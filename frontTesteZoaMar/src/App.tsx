f9c8c8bd6d1110d729a910fb773800be03d70fa6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-05-10 16:38:26 -0300

    4upTeste

--- frontTesteZoaMar/src/App.tsx
+++ frontTesteZoaMar/src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/header";
 import { SubHeader } from "./components/subHeader";
+import { UserPage } from "./user";
 
 export function App() {
     return(
         <div className="">
             <Header /> 
             <SubHeader />
+            <UserPage />
         </div>
 
         
---
+++ frontTesteZoaMar/src/user.tsx
@@ -0,0 +1,5 @@
+export function UserPage(){
+    return(
+        <h1></h1>
+    )
+}
