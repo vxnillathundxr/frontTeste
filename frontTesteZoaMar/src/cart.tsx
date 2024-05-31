e288dc9d014752025c773400db546bb1ce63cd71
vxnillathundxr <alexandrejuraski@hotmail.com>
2024-05-31 00:51:29 -0300

    InputTesteQuasePronto

--- frontTesteZoaMar/src/cart.tsx
+++ frontTesteZoaMar/src/cart.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/header";
 import allStar from './assets/allStarIcon.svg'
+import BotaoParaInput from "./components/search";
 
 export function CartPage(){
     return(
         <div>
             <Header />
             <h1>Cart</h1>
+            <BotaoParaInput />
         </div>
     )
 }
--- frontTesteZoaMar/src/components/header.tsx
+++ frontTesteZoaMar/src/components/header.tsx
@@ -2,41 +2,50 @@ import React, {useState} from 'react'
 import BotaoParaInput from './search'
 import BotaoParaMenu from './menu'
 import ExpandableMenu from './menu'
-
-
 import cartIcon from '../assets/cartIcon.svg' 
 import userIcon from '../assets/userIcon.svg'
 import { Search, ShoppingCart, UserRound } from 'lucide-react'
 // import menuIcon from '../assets/menuIcon.svg'
-
 export function Header(){
     return(
             // div principal
-            <div className="w-full bg-black h-12 gap-2 p-5 flex flex-row justify-between pb-10">
-             {/*div IconMenu e ZoaMar  */}
-                <div className="flex pt-3">
-                    <nav className="flex items-center">
-                        <div className="m-0 p-0">
-                        {/* <BotaoParaMenu /> */}
-                        <ExpandableMenu />
-                        {/* <a href=""><img src={menuIcon} /></a> */}
+            <div className="w-full bg-black h-14">
+                    <div className="gap-2 p-5 flex justify-between py-0 items-center">
+                    {/*div IconMenu e ZoaMar  */}
+                        <div className="flex pt-3">
+                            <nav className="flex items-center">
+                                <div className="hidden max-md:block">
+                                {/* <BotaoParaMenu /> */}
+                                <ExpandableMenu />
+                                {/* <a href=""><img src={menuIcon} /></a> */}
+                                </div>
+                                <a href={'/'} className="text-white text-2xl">ZoaMar</a>
+                            </nav>
+                            {/* divCertesAbas */}
                         </div>
-                        <a href={'/'} className="text-white text-2xl pb-1">ZoaMar</a>
-                    </nav>
+                            <div className="hidden md:block">
+                                <nav className="flex gap-20 items-center justify-center mt-7">
+                                    <a className="text-white" href='/camisetas'>Camisetas</a>
+                                    <a className="text-white" href='/moletons'>Moletons</a>
+                                    <a className="text-white" href='/calcas'>Calças</a>
+                                    <a className="text-white" href='/tenis'>Tênis</a>
+                                </nav>
+                            </div>
+                        {/* divs left */}
+                        {/* div searchIcon */}
+                            
+                            {/* userIcon e cartIcon */}
+                            <div className="flex gap-5">
+                                    {/* <img src={searchIcon}  id="icone" className="size-6"/> */}
+                                <nav className="flex gap-4 mt-3">
+                                    <a className='text-white' href='/user'><UserRound/></a>
+                                    <a className='text-white' href='/cart'><ShoppingCart/></a>
+                                </nav>
+                            </div>
+                    </div>
+                <div className="pt-10 right-0">
+                    <BotaoParaInput />         
                 </div>
-                {/* divs left */}
-                    <div className="flex gap-5 pb-">
-                {/* div searchIcon */}
-                    </div>
-                    {/* userIcon e cartIcon */}
-                    <div className="flex gap-5">
-                            <BotaoParaInput />
-                            {/* <img src={searchIcon}  id="icone" className="size-6"/> */}
-                        <nav className="flex gap-4">
-                            <a className='text-white' href='/user'><UserRound/></a>
-                            <a className='text-white' href='/cart'><ShoppingCart/></a>
-                        </nav>
-                    </div>
             </div>
     )
 }
