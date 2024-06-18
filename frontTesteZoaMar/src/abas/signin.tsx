2aa3283ac98e472be3c68da4245f8edcf40a5843
vxnillathundxr <alexandrejuraski@hotmail.com>
2024-06-17 23:14:42 -0300

    testeConect02SemSucesso

--- frontTesteZoaMar/src/abas/signin.tsx
+++ frontTesteZoaMar/src/abas/signin.tsx
@@ -1,7 +1,47 @@
 import { EndH } from "../components/endH"
 import { Header } from "../components/header"
+import { api } from '../services/api'
+import { useEffect, useState, useRef, FormEvent } from 'react'
+
+interface CustomerProps{
+    id: string;
+    email: string;
+    password: string;
+    
+}
 
 export function SignIn(){
+    
+    const [customers, setCustomers] = useState<CustomerProps[]>([])
+    const emailRef = useRef<HTMLInputElement | null>(null)
+    const passwordRef = useRef<HTMLInputElement | null>(null)
+
+    useEffect(() => {
+        loadCustomers();
+    }, [])
+    
+    async function loadCustomers(){
+        const response = await api.get("/logins")
+        setCustomers(response.data);
+    }
+    
+    async function handleSubmit(event: FormEvent){
+        event.preventDefault();
+    
+    if(!emailRef.current?.value || passwordRef.current?.value) return;
+    
+    const response = await api.post("/login", {
+        email: emailRef.current?.value,
+        password: passwordRef.current?.value
+
+    })
+    console.log(response.data);
+    
+        // console.log(emailRef.current?.value)
+        // console.log(passwordRef.current?.value)
+    }
+    
+
     return(
         <div>
             <Header/>
@@ -11,29 +51,44 @@ export function SignIn(){
                         <h1>CADASTRAR</h1>
                     </div>
                     <div className="flex flex-col h-auto w-full justify-center p-5 text-white">
-                        <div className="flex h-auto w-full justify-center p-5">
+                        {/* <div className="flex h-auto w-full justify-center p-5">
                             <input className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  placeholder="Nome" type="text" />
-                        </div>
-                        <div className="flex h-auto w-full justify-center p-5">
-                            <input className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  placeholder="E-Mail" type="text" />
-                        </div>
-                        <div className="flex h-auto w-full justify-center p-5">
-                            <input className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  placeholder="Telefone" type="number" />
-                        </div>
-                        <div className="flex h-auto w-full justify-center p-5">
-                            <input className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  placeholder="Senha" type="text" />
-                        </div>
-                        <div className="flex h-auto w-full justify-center p-5">
+                        </div> */}
+                        <form onSubmit={handleSubmit}>
+                            <div className="flex h-auto w-full justify-center p-5">
+                                <input 
+                                className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  
+                                placeholder="E-Mail" 
+                                type="text" 
+                                ref={emailRef}
+                                />
+                            </div>
+                            {/* <div className="flex h-auto w-full justify-center p-5">
+                                <input className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  placeholder="Telefone" type="number" />
+                            </div> */}
+                            <div className="flex h-auto w-full justify-center p-5">
+                                <input 
+                                className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  
+                                placeholder="Senha" 
+                                type="text" 
+                                ref={passwordRef}
+                                />
+                            </div>
+                            <div className="flex h-auto w-full justify-center p-5">
+                                <input 
+                                type="submit"
+                                value="Cadastrar"
+                                className="cursor-pointer w-96 p-2 bg-black rounded font-medium text-white" />
+                            </div>
+                        </form>
+                        {/* <div className="flex h-auto w-full justify-center p-5">
                             <input className="bg-zinc-500 placeholder:text-white rounded w-96 p-2"  placeholder="Confirmar Senha" type="text" />
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-32 items-center">
-                        <div className=" flex bg-black text-white p-3 rounded w-24 h-10 items-center justify-center">
-                            <button>CRIAR</button>
-                        </div>
-                        <div className="flex gap-2">
-                            <a>Já tem conta? </a>
-                            <a href='/User' > Clique aqui para logar</a>
+                        </div> */}
+                        <div className="flex flex-col gap-32 items-center">
+                            <div className="flex gap-2 text-black">
+                                <a>Já tem conta? </a>
+                                <a href='/User' > Clique aqui para logar</a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- frontTesteZoaMar/src/abas/user.tsx
+++ frontTesteZoaMar/src/abas/user.tsx
@@ -1,66 +1,77 @@
 import { EndH } from "../components/endH";
 import { Header } from "../components/header";
 import { UserRound } from "lucide-react";
-import {api} from '../services/api'
-import { useEffect, useState, useRef } from 'react'
+// import { api } from '../services/api'
+// import { useEffect, useState, useRef, FormEvent } from 'react'
 
-interface CustomerProps{
-    id: string;
-    email: string;
-    password: string;
+// interface CustomerProps{
+//     id: string;
+//     email: string;
+//     password: string;
 
-}
+// }
 
 
-export function UserPage(){
 
-    const [customers, setCustomers] = useState<CustomerProps[]>([])
+// const [customers, setCustomers] = useState<CustomerProps[]>([])
+// const emailRef = useRef<HTMLInputElement | null>(null)
+// const passwordRef = useRef<HTMLInputElement | null>(null)
 
-    useEffect(() => {
-        loadCustomers();
-    }, [])
-
-    async function loadCustomers(){
-        const response = await api.get("/logins")
-        setCustomers(response.data);
-    }
-
-    return(
-        <div>
-            <Header />
-            <div className="flex flex-col gap-96">
-                {customers.map( (customer) =>  (
-                    <div className="flex justify-center items-center pt-20">
-                        key={customer.id}
-                       <div className="md:flex md:flex-col md:bg-zinc-300 md:p-6 md:size-auto md:rounded md:items-center md:gap-5
-                                       flex flex-col bg-zinc-300 h-[60vh] w-[90vw] p-6 rounded items-center gap-5">
-                           <div className="flex justify-between w-[85vw] md:w-80">
-                               <h1 className="font-bold text-2xl ">Entrar na Conta</h1>
-                               <UserRound className="size-16"/> 
-                           </div>
-                           <div className="flex flex-col gap-10 items-center">
-                               <div>
-                                   <h2 className="font-bold text-xl">E-mail</h2>
-                                   <p><input className="bg-zinc-500 rounded w-[85vw] md:w-80 p-2 text-white" type="text" />{customer.email}</p>
-                               </div>
-                               <div>
-                                   <h2 className="font-bold text-xl">Senha</h2>
-                                   <p><input className="bg-zinc-500 rounded w-[85vw] md:w-80 p-2 text-white" type="text" />{customer.password}</p>
-                               </div>
-                           </div>
-                           <div className="pt-12">
-                               <button className=" bg-black text-white w-40 h-12 rounded text-xl">Entrar</button>
-                           </div>
-                           <div className="pt-12 flex justify-between w-64 mb:w-80">
-                               <a className="font-medium">Esqueci minha senha</a>
-                               <a href='/Signin' className="font-medium">Cadastrar</a>
-                           </div>
-                       </div>
-                   </div>
-                ))}
+// useEffect(() => {
+    //     loadCustomers();
+    // }, [])
+    
+    // async function loadCustomers(){
+        //     const response = await api.get("/logins")
+        //     setCustomers(response.data);
+        // }
+        
+        // async function handleSubmit(event: FormEvent){
+            //     event.preventDefault();
+            
+            // if(!emailRef.current?.value || passwordRef.current?.value) return;
+            
+            // const response = await api.post("/login", {
+                //     email: emailRef.current?.value,
+                //     password: passwordRef.current?.value
+                // })
+                
+                //     // console.log(emailRef.current?.value)
+                //     // console.log(passwordRef.current?.value)
+                // }
+                
+    export function UserPage(){
+        return(
+            <div>
+                <Header />
+                        <div className="flex justify-center items-center pt-20">
+                        <div className="md:flex md:flex-col md:bg-zinc-300 md:p-6 md:size-auto md:rounded md:items-center md:gap-5
+                                        flex flex-col bg-zinc-300 h-[60vh] w-[90vw] p-6 rounded items-center gap-5">
+                            <div className="flex justify-between w-[85vw] md:w-80">
+                                <h1 className="font-bold text-2xl ">Entrar na Conta</h1>
+                                <UserRound className="size-16"/> 
+                            </div>
+                            <div className="flex flex-col gap-10 items-center">
+                                <div>
+                                    <h2 className="font-bold text-xl">E-mail</h2>
+                                    <input className="bg-zinc-500 rounded w-[85vw] md:w-80 p-2 text-white" type="text" />
+                                </div>
+                                <div>
+                                    <h2 className="font-bold text-xl">Senha</h2>
+                                    <input className="bg-zinc-500 rounded w-[85vw] md:w-80 p-2 text-white" type="text" />
+                                </div>
+                            </div>
+                            <div className="pt-12">
+                                <button className=" bg-black text-white w-40 h-12 rounded text-xl">Entrar</button>
+                            </div>
+                            <div className="pt-12 flex justify-between w-64 mb:w-80">
+                                <a className="font-medium">Esqueci minha senha</a>
+                                <a href='/Signin' className="font-medium">Cadastrar</a>
+                            </div>
+                        </div>
+                    </div>
                 <EndH />
             </div>
-        </div>
     )
 }
 
